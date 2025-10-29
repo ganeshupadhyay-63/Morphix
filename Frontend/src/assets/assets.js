b4364c2285161ec5e4e0aad5888555cf59cd0bed
Ganesh Upadhyay <ganeshupadhyay729@gmail.com>
2025-10-29 11:56:09 +0530

    Update assets.js

--- Frontend/src/assets/assets.js
+++ Frontend/src/assets/assets.js
@@ -1,7 +1,7 @@
 
 import gradientBackground from "./gradientBackground.png";
 import user_group from "./user_group.png";
-import star_icon from "./star_icon.svg";
+
 import star_dull_icon from "./star_dull_icon.svg";
 import profile_img_1 from "./profile_img_1.png";
 import arrow_icon from "./arrow_icon.svg";
@@ -14,7 +14,7 @@ export const assets = {
 
     gradientBackground,
     user_group,
-    star_icon,
+   
     star_dull_icon,
     profile_img_1,
     arrow_icon,
